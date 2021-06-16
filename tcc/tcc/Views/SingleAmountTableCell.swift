e627c61668014f9b3e757bd93d76cb9ba2310d92
cassiaabarbosa <cassia.a.barbosa@gmail.com>
2021-06-15 21:26:00 -0300

    Change tableView.s cells

--- tcc/tcc/Views/SingleAmountTableCell.swift
+++ tcc/tcc/Views/SingleAmountTableCell.swift
@@ -5,15 +5,10 @@ class SingleAmountTableCell: UITableViewCell {
     static var id: String {
         return String(describing: self)
     }
-
-    @TemplateView var amount: UILabel
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        buildViewHierarchy()
-        applyConstraints()
         cellAttributes()
-        setupAmount()
     }
     
     @available(*, unavailable)
@@ -21,35 +16,19 @@ class SingleAmountTableCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func buildViewHierarchy() {
-        contentView.addSubview(amount)
-    }
-    
     private func cellAttributes() {
         backgroundColor = .tccBlack
-    }
-    
-    private func setupAmount() {
-        amount.textColor = .white
-        amount.textAlignment = .center
-        amount.font = UIFont.boldSystemFont(ofSize: 20)
-        amount.numberOfLines = 0
-    }
-    
-    private func applyConstraints() {
-        NSLayoutConstraint.activate([
-            amount.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            amount.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
-            amount.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            amount.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
-        ])
+        textLabel?.textColor = .white
+        textLabel?.textAlignment = .center
+        textLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        textLabel?.numberOfLines = 0
     }
     
     func show(value: Int, amount: Int) {
         if amount > 1 {
-            self.amount.text = "Há \(amount) notas de R$ \(value),00"
+            self.textLabel?.text = "Há \(amount) notas de R$ \(value),00"
         } else {
-            self.amount.text = "Há \(amount) nota de R$ \(value),00"
+            self.textLabel?.text = "Há \(amount) nota de R$ \(value),00"
         }
     }
 }
